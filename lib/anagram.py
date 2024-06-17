1489ab1e4d9dc31632802e1f9d881224c594c267
splitlang <seanmark.student@moringaschool>
2024-06-17 16:37:38 +0300

    completed

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,17 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def match(self, possible_anagrams):
+        matches = []
+        sorted_word = sorted(self.word)
+
+        for anagram in possible_anagrams:
+            anagram = anagram.lower()
+            sorted_anagram = sorted(anagram)
+
+            if sorted_word == sorted_anagram:
+                matches.append(anagram)
+
+        return matches
